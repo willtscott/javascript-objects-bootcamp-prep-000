a54da5dacea7e2042dbeba3fbdaf82d77f78c30f
wtscott@gmail.com <wtscott@gmail.com>
2018-04-21 01:24:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,6 @@ var playlist = new Object({cranberries: "linger"})
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist}
